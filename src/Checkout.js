234d162248143c99a8cf6994ea5b19c2e6554844
Jonathan <jonathan.gb001@gmail.com>
2022-03-07 02:12:47 -0500

    added css

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,24 +1,27 @@
 import React from "react";
 
 function Checkout({ checkoutItem, handleRemoveFromCheckout }) {
-
   const listCheckoutItem = checkoutItem.map((item) => {
     return (
-      <li>
-        <p>{item.name} {item.count} - {item.totalPrice}</p>
-        <button onClick={() => handleRemoveFromCheckout(item.name)} >Remove</button>
+      <li key={item.name}>
+        <p>
+          {item.name} {item.count}x - ${item.totalPrice.toFixed(2)}
+        </p>
+        <button onClick={() => handleRemoveFromCheckout(item.name)}>
+          Remove
+        </button>
       </li>
     );
   });
 
   const total = checkoutItem.reduce((total, item) => {
-    return total + item.totalPrice
-  }, 0)
+    return total + item.totalPrice;
+  }, 0);
 
   return (
-    <div className="Checkout-cart">
+    <div className="checkout-cart">
       <ul>{listCheckoutItem}</ul>
-      <p>Total: {total}</p>
+      <p>Total: ${total.toFixed(2)}</p>
     </div>
   );
 }
--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Item({ name, price, id, handleAddToCart }) {
-  const [itemCount, setItemCount] = React.useState(0);
+function Item({ image, name, price, description, handleAddToCart }) {
+  const [itemCount, setItemCount] = React.useState(1);
 
   function increment() {
     setItemCount((prevCount) => {
@@ -9,22 +9,61 @@ function Item({ name, price, id, handleAddToCart }) {
     });
   }
 
-  function addToCart() {
-    handleAddToCart(itemCount, itemCount * price, name);
-    setItemCount(0)
+  function substract() {
+    setItemCount((prevCount) => {
+      return prevCount > 1 ? prevCount - 1 : 1;
+    });
   }
 
+  function addToCart() {
+    if (itemCount > 0) {
+      handleAddToCart(itemCount, itemCount * price, name);
+      setItemCount(1);
+    } else {
+      alert("you must add at least one item to the cart");
+    }
+  }
+
+  const newLocal = (
+    <button disabled className="counter-btn" onClick={substract}>
+      -
+    </button>
+  );
   return (
     <div className="item-card">
-      <h1>{name}</h1>
-      <p>{price}</p>
-      <p>Amount: {itemCount}</p>
-      <button onClick={increment}>Increment</button>
-      <p>total: {itemCount * price}$</p>
-      <button
-        onClick={addToCart}
-      >
-        Add to cart
+      <img className="item-img" src={image} />
+      <div className="item-title">
+        <h1>{name}</h1>
+        <h1>${price.toFixed(2)}</h1>
+      </div>
+      <p>{description}</p>
+      <div className="item-counter">
+        {itemCount <= 1 && (
+          <button
+            className="counter-btn noselect minus-btn"
+            onClick={substract}
+            disabled
+          >
+            <i class="fa-solid fa-minus"></i>
+          </button>
+        )}
+        {itemCount > 1 && (
+          <button
+            className="counter-btn noselect minus-btn"
+            onClick={substract}
+          >
+            <i class="fa-solid fa-minus"></i>
+          </button>
+        )}
+        <div className="counter-display">
+          <h1>{itemCount}</h1>
+        </div>
+        <button className="counter-btn noselect plus-btn" onClick={increment}>
+          +
+        </button>
+      </div>
+      <button className="item-add-btn" onClick={addToCart}>
+        Add {itemCount} to cart &bull; ${(itemCount * price).toFixed(2)}
       </button>
     </div>
   );
